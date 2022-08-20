b89df014c5658eaea7c763e62a79341349ec3f46
Clément Renault <clement@meilisearch.com>
2022-08-20 11:03:40 +0200

    Import the default bevy template

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_startup_system(setup)
-        .run();
+    App::new().add_plugins(DefaultPlugins).add_startup_system(setup).run();
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
